81e88e619227eb37e63b682ec63db481bb32f469
Juliana Costa <julianacosta_s@hotmail.com>
2022-06-08 21:28:51 +1000

    Put sound into the C file

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import AVFoundation // Audio Visio foundation allows interaction with different parts of the hardware
 
 class ViewController: UIViewController {
+   
+    //declare variable for the sound
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav") //location of the C file with extension of wav
+        player = try! AVAudioPlayer(contentsOf: url!) //The url file is put inside this player
+        player.play() // tell the player to play the sound
+                
+    }
 }
-
